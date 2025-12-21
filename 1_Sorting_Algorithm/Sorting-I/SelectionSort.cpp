e5f339d851aea43d3091bc837a6e79217c47bc81
Abhay Soni <abhaysoni512@gmail.com>
2025-12-21 15:11:13 +0530

    Update SelectionSort.cpp - 2025-12-21 15:11:13

--- 1_Sorting_Algorithm/Sorting-I/SelectionSort.cpp
+++ 1_Sorting_Algorithm/Sorting-I/SelectionSort.cpp
@@ -1,7 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define Swap(a,b) {int temp = a; a = b; b = temp;}
+
+void SelectionSort(int arr[], int n){
+    for(int i=0; i<=n-2; i++){
+        int min = i;
+        for(int j=i+1;j<=n-1; j++){
+            if(arr[j]<arr[min]) min = j;
+        }
+        Swap(arr[i], arr[min]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
 int main(){
-    
+    int arr[6] = {13, 46, 24, 52, 20, 9};
+    SelectionSort(arr, 6);
     return 0;
 }
