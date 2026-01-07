9c432e6106451188053bfdf3e65b672e21237ea5
Abhay Soni <abhaysoni512@gmail.com>
2026-01-07 14:58:36 +0530

    Update p1_reverseString.cpp - 2026-01-07 14:58:36

---
+++ 1_Strings/p1_reverseString.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int len(char c[]) ;
+
+void reverseString(char c[])
+{
+    int s=0;
+    int l=len(c)-1;
+
+    while (s<l)
+    {
+        swap(c[s],c[l]);
+    }
+    
+}
+
+int len(char c[]){
+    int count=0;
+    for(int i=0;c[i]!='\0';i++ ){
+        count++;
+    }
+    return count;
+}
+int main(){
+    char c[100];
+    cin.getline(c,100);
+
+    cout<<strlen(c);
+
+    return 0;
+}
