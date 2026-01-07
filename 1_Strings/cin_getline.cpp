1d2bbca66084849ab9ec74efd26eb092462f8e5b
Abhay Soni <abhaysoni512@gmail.com>
2026-01-07 14:41:48 +0530

    Update cin_getline.cpp - 2026-01-07 14:41:48

--- 1_Strings/cin_getline.cpp
+++ 1_Strings/cin_getline.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 int main(){
     char b[100];
-    cin.getline(b,100);
+    //cin.getline(b,100);   // inp         ---> output
+    cin.getline(b,100,'o'); // hello World ---> hell
     cout<<b;
     return 0;
 }
