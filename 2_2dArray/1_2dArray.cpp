36ec10b283ddc81165ef89143ca9e6a005130658
Abhay Soni <abhaysoni512@gmail.com>
2026-01-08 13:44:05 +0530

    Update 1_2dArray.cpp - 2026-01-08 13:44:05

--- 2_2dArray/1_2dArray.cpp
+++ 2_2dArray/1_2dArray.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void printElement(int arr[][4],int m, int n){
     for(int i=0; i<m; i++){
         for(int j=0; j<n; j++){
-            cout<<arr[m][n];
+            cout<<arr[i][j]<<" ";
         }
         cout<<endl;
     }
