b9506267180d1d8b565126c7b1a79080e2abb506
Abhay Soni <abhaysoni512@gmail.com>
2026-01-08 14:08:31 +0530

    Update string.cpp - 2026-01-08 14:08:31

---
+++ 3_Strings/string.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s = "abc";
+    cout<<s<<endl;
+
+    string *sp = new string ;
+    *sp = "pqr";
+    cout<<sp<<endl<<*sp<<endl;
+    return 0;
+}
