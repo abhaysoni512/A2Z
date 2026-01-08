85fe0527b02583ab1980e9580de0d4192d9eb839
Abhay Soni <abhaysoni512@gmail.com>
2026-01-08 10:47:03 +0530

    Update p2_printAllPrefix.cpp - 2026-01-08 10:47:03

--- 1_Strings/p2_printAllPrefix.cpp
+++ 1_Strings/p2_printAllPrefix.cpp
@@ -1,13 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-void prefixPrint(char c[]){
-    size_t l = strlen(c)-1; //3
+void prefixPrint(char c[])
+{
+    size_t l = strlen(c); // 3
     size_t i = 0;
-    for(int i = 0; i<l; i++){
-        for(int j = 0; j<=i;j++){
-            cout<<c[j];
+    for (int i = 0; i < l; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << c[j];
         }
-        cout<<endl;
+        cout << endl;
     }
 }
 
