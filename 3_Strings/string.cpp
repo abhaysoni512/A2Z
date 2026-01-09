848934ae5ed131d384e61f8d45350a94a55c0330
Abhay Soni <abhaysoni512@gmail.com>
2026-01-09 14:41:40 +0530

    Update string.cpp - 2026-01-09 14:41:40

--- 3_Strings/string.cpp
+++ 3_Strings/string.cpp
@@ -1,22 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     string s = "abc";
-    cout<<s<<endl;
+    cout << s << endl;
 
-    string *sp = new string ;
+    string *sp = new string;
     *sp = "pqr";
-    cout<<sp<<endl<<*sp<<endl;
-
+    cout << sp << endl
+         << *sp << endl;
 
     // taking input in string
     string s2;
     getline(cin, s2); //
-    cout<<s2<<endl;
+    cout << s2 << endl;
 
     string s3 = "pqrabcabc";
-    cout<<s3.find("abc")<<endl;
+    cout << s3.find("abc") << endl;
+
+    int a = 134;
+    string s4 = to_string(a);
+
+    cout << typeid(s4).name() << endl;
+
+    s4[0] = '9';
+    a = atoi(s4.c_str());
+    cout << a << endl;
 
     return 0;
 }
