48fdae7e23811eba6174ee6862cf87c075325448
Abhay Soni <sonia@avaya.com>
2026-01-12 16:38:38 +0530

    Update vector.cpp - 2026-01-12 16:38:38

--- 6_Vectors/vector.cpp
+++ 6_Vectors/vector.cpp
@@ -12,14 +12,15 @@ int main() {
     cout<<v->at(2)<<endl;
 
     try {
-    v->at(4) = 40;   // will throw std::out_of_range because size is 3
-} catch (const out_of_range& e) {
-    cout << "out_of_range: " << e.what() << "\n";
+        v->at(4) = 40;   // will throw std::out_of_range because size is 3
+    } catch (const out_of_range& e) {
+        cout << "out_of_range: " << e.what() << "\n";
 }
     cout<<(*v)[4]<<endl;
 
-    cout<<"size = "<<v->size()<<endl;
 
+
+    
 
     return 0;
 }
