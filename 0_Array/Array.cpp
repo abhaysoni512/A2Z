0ddcc9c2b3594f8526cfbb509695faa0a7632d26
Abhay Soni <abhaysoni512@gmail.com>
2026-03-05 16:39:25 +0530

    Update Array.cpp - 2026-03-05 16:39:25

---
+++ 0_Array/Array.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct M_Array{
+    int *A; // pointer to array in heap
+    int size; // size of array (capacity)
+    int length; // number of elements in array (currently present)
+};
+
+void display(const M_Array &m){
+    int i = 0;
+    cout<<"Elements in array:- ";
+    for(;i<m.length;i++){
+        cout<<m.A[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    M_Array arr;
+    int n,i;
+    cout<<"Enter the size of array :";
+    cin>>arr.size;
+    arr.A = new int[arr.size];
+    arr.length = 0;
+
+    cout<<"Enter all the elements"<<endl;
+    for(i=0;i<arr.size;i++){
+        cin>>arr.A[i];
+        arr.length++;
+    }
+
+    display(arr);
+    
+
+
+    return 0;
+}
