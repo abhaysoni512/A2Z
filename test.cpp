a45fbab348f378a4fa3609c954c2e7ce8ab0dfda
Abhay Soni <abhaysoni512@gmail.com>
2026-03-28 20:57:35 +0530

    Update test.cpp - 2026-03-28 20:57:35

--- test.cpp
+++ test.cpp
@@ -1,22 +1,54 @@
 #include <iostream>
 
-class Foo {
+class Fraction
+{
+private:
+    int m_numerator;
+    int m_denominator;
+
 public:
-    int m_x;
+    // Default constructor
+    Fraction(int numerator = 0, int denominator = 1) : m_numerator{numerator}, m_denominator{denominator} {}
 
-    Foo(int x) : m_x(x) {}  // Parameterized constructor declared → no implicit default ctor.
-
-    // Foo() {}  // This would be user-provided (non-trivial).
-    Foo() = default;  // Explicitly defaulted: Compiler generates it (trivial if possible).
-
-    void print() const {
-        std::cout << "Foo(" << m_x << ")\n";
+    // Copy constructor
+    Fraction(const Fraction &other) : m_numerator{other.m_numerator}, m_denominator{other.m_denominator}
+    {
+        std::cout << "Copy constructor called\n";
     }
-};
 
-int main() {
-    Foo f1;      // OK: Calls the defaulted default ctor (m_x default-initialized to indeterminate value).
-    f1.print();  // May print an indeterminate value for m_x.
-    Foo f2(42);  // OK: Calls the parameterized ctor.
+    // Copy assignment operator
+    Fraction &operator=(const Fraction &other);
+
+    friend std::ostream &operator<<(std::ostream &out, const Fraction &f);
+};
+std::ostream &operator<<(std::ostream &out, const Fraction &f)
+{
+    out << f.m_numerator << '/' << f.m_denominator;
+    return out;
+}
+
+Fraction &Fraction::operator=(const Fraction &other)
+{
+    std::cout << "Copy assignment operator called\n";
+    if (this == &other)
+    {                 // check for self-assignment
+        return *this; // return the current object to allow chaining of assignment operations
+    }
+    m_numerator = other.m_numerator;     // copy the numerator from the other object
+    m_denominator = other.m_denominator; // copy the denominator from the other object
+    return *this;                        // return the current object to allow chaining of assignment operations
+}
+
+int main()
+{
+    Fraction f1{5, 3};
+    Fraction f2{7, 2};
+    Fraction f3{9, 5};
+
+    f1 = f2 = f3; // chained assignment
+
     return 0;
 }
+
+
+
