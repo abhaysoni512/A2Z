f20c05a1b479e1750bd1e49e502c5d218ffab5a5
Abhay Soni <abhaysoni512@gmail.com>
2026-05-30 15:21:26 +0530

    Update test.cpp - 2026-05-30 15:21:26

--- test.cpp
+++ test.cpp
@@ -1,31 +1,51 @@
 #include <iostream>
-#include <memory>
 
-class Resource2; // forward declaration of Resource2
-
-class Resource1
+class A
 {
 public:
-    std::shared_ptr<Resource2> m_ptr2; // shared_ptr to Resource2
-    Resource1() { std::cout << "Resource1 acquired\n"; }
-    ~Resource1() { std::cout << "Resource1 destroyed\n"; }
+    A(int a)
+    {
+        std::cout << "A: " << a << '\n';
+    }
+    ~A()
+    {
+        std::cout << "Destroying A\n";
+    }
 };
 
-class Resource2
+class B : public A
 {
 public:
-    std::weak_ptr<Resource1> m_ptr1; // weak_ptr to Resource1
-    Resource2() { std::cout << "Resource2 acquired\n"; }
-    ~Resource2() { std::cout << "Resource2 destroyed\n"; }
+    B(int a, double b)
+        : A{a}
+    {
+        std::cout << "B: " << b << '\n';
+    }
+
+    ~B()
+    {
+        std::cout << "Destroying B\n";
+    }
+};
+
+class C : public B
+{
+public:
+    C(int a, double b, char c)
+        : B{a, b}
+    {
+        std::cout << "C: " << c << '\n';
+    }
+
+    ~C()
+    {
+        std::cout << "Destroying C\n";
+    }
 };
 
 int main()
 {
-    std::shared_ptr<Resource1> p1{new Resource1}; // create a shared_ptr to Resource1
-    std::shared_ptr<Resource2> p2{new Resource2}; // create a shared_ptr to Resource2
-
-    p1->m_ptr2 = p2; // p1's m_ptr2 is a shared_ptr to p2, so Resource1 has a shared_ptr to Resource2
-    p2->m_ptr1 = p1; // p2's m_ptr1 is a weak_ptr to p1, so Resource2 has a weak_ptr to Resource1, breaking the circular reference
+    C c{5, 4.3, 'R'};
 
     return 0;
 }
